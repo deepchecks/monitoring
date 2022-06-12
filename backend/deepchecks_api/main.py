0461d0dd6b5c0d1a74966c04ab411a34a30f1806
JKL98ISR <jonatan.lib@gmail.com>
2022-06-12 11:34:07 +0300

    v0

--- backend/deepchecks_api/main.py
+++ backend/deepchecks_api/main.py
@@ -33,12 +33,3 @@ def get_db():
 #     )
 
 app.include_router(router, prefix="/api/v1")
-
-@app.post("/models/", response_model=schemas.Model)
-async def create_item(model: schemas.Model, db: Session = Depends(get_db)):
-    data = model.dict(exclude_none=True)
-    db_item = models.Model(**data)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
