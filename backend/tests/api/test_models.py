46d0c7c7baa9eb8f4357315a6e51429b94dc9b7e
Yuriy Romanyshyn <yuriy.romanyshyn.lv.ua@gmail.com>
2022-07-08 17:02:33 +0300

    tests modification

--- backend/tests/api/test_models.py
+++ backend/tests/api/test_models.py
@@ -1,12 +1,7 @@
-import pytest
-from httpx import AsyncClient
 
 
-pytestmark = pytest.mark.anyio
-
-
-async def test_add_stuff(client: AsyncClient):
-    response = await client.post("/api/v1/models", json={"name": "44", "task_type": "classification"})
+def test_add_stuff(client):
+    response = client.post("/api/v1/models", json={"name": "44", "task_type": "classification"})
     assert response.status_code == 200
     assert response.json() == {"id": 1, "name": "44", "description": None, "task_type": "classification"}
 
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,60 +3,48 @@ import typing as t
 import pytest
 import pytest_asyncio
 import testing.postgresql
-from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import create_async_engine
 
-from deepchecks_monitoring.dependencies import AsyncSessionDep
 from deepchecks_monitoring.app import create_application
 from deepchecks_monitoring.models.base import Base
 
 
-@pytest.fixture(
-    params=[
-        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
-    ]
-)
-def anyio_backend(request):
-    return request.param
+# @pytest.fixture(
+#     params=[
+#         pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
+#     ]
+# )
+# def anyio_backend(request):
+#     return request.param
+
+
+@pytest.fixture(scope='session')
+def postgres():
+    with testing.postgresql.Postgresql(port=7654) as postgres:
+        yield postgres
 
 
 @pytest_asyncio.fixture()
-async def engine():
-    with testing.postgresql.Postgresql(port=7654) as postgres:
-        async_url = postgres.url().replace('postgresql', 'postgresql+asyncpg')
-        engine = create_async_engine(async_url, future=True, echo=True)
+async def engine(postgres):
+    async_url = postgres.url().replace('postgresql', 'postgresql+asyncpg')
+    engine = create_async_engine(async_url, future=True, echo=True)
 
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
-        yield engine
-        await engine.dispose()
+    yield engine
+    await engine.dispose()
 
 
 @pytest.fixture()
 def application(engine):
     application = create_application(database_engine=engine)
-    # async def override_get_db():
-    #     TestingSessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
-    #     async with TestingSessionLocal() as session:
-    #         try:
-    #             yield session
-    #             await session.commit()
-    #         except Exception:
-    #             await session.rollback()
-    #         finally:
-    #             await session.close()
-    # application.dependency_overrides[AsyncSessionDep.dependency] = override_get_db
     return application
 
 
-@pytest_asyncio.fixture()
-async def client(application) -> t.AsyncIterator[AsyncClient]:
-    async with AsyncClient(
-        app=application,
-        base_url="http://testserver",
-        headers={"Content-Type": "application/json"},
-    ) as client:
+@pytest.fixture()
+def client(application) -> t.Iterator[TestClient]:
+    with TestClient(app=application) as client:
         yield client
