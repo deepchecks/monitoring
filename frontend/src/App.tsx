6d38010d8fdf9c15cac9b47b1200f59b7cd8e16b
Itay Gabbay <itay@deepchecks.com>
2022-08-29 14:00:13 +0300

    Switching to HashRouter (#182)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import { theme } from "./helpers/theme";
 import MyRouts from "./routes";
@@ -11,14 +11,14 @@ import { store } from "./store/store";
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <CssBaseline />
             <MyRouts />
           </LocalizationProvider>
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
