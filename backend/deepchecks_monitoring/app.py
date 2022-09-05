bbe2694dc2db70d87520858753dc7b615b08de86
Matan Perlmutter <matan@deepchecks.com>
2022-09-05 18:32:52 +0300

    comment pg instrument

--- backend/deepchecks_monitoring/app.py
+++ backend/deepchecks_monitoring/app.py
@@ -204,6 +204,6 @@ def create_application(
         # Add telemetry
         if settings.instrument_telemetry:
             FastAPIInstrumentor.instrument_app(app)
-            AsyncPGInstrumentor().instrument()
+            # AsyncPGInstrumentor().instrument()
 
     return app
