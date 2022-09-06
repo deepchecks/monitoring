18b864c98f6a875be6458a15d9525e994cfcc668
kostaWebiya <111682342+kostaWebiya@users.noreply.github.com>
2022-09-06 10:21:52 +0300

    Add https=true to start script & minor refactors to axios instance

--- frontend/src/services/customAxios.ts
+++ frontend/src/services/customAxios.ts
@@ -1,15 +1,17 @@
-import axios from 'axios';
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios';
-// export const AXIOS_INSTANCE = Axios.create({ baseURL: 'https://mon-demo-083122.deepchecks.com/' });
-// export const AXIOS_INSTANCE = Axios.create({ baseURL: 'http://mon-commercial-283366795.eu-west-1.elb.amazonaws.com/' });
-export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.REACT_APP_BASE_API , withCredentials: true});
+export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.REACT_APP_BASE_API, withCredentials: true });
+
 // Adding an axios interceptor that will handle unauthorized users & users with incomplete details:
 AXIOS_INSTANCE.interceptors.response.use(
   response => response,
   error => {
     const { response } = error;
     if (response.status === 401) {
-      window.location.href = 'https://staging-v2.deepchecks.com/api/v1/auth/login/auth0?return_uri=https://localhost:3000';
+      window.location.href = `${
+        process.env.REACT_APP_BASE_API
+      }/api/v1/auth/login/auth0?return_uri=${encodeURIComponent(`
+        ${process.env.REACT_APP_LOCAL_URL}
+      `)}`;
     } else if (response.status === 403 && response.headers['x-substatus'] === '10') {
       console.log('le response::', response);
       // Complete details...
@@ -33,17 +35,3 @@ export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
 
 // In some case with react-query and swr you want to be able to override the return error type so you can also do it here like this
 export type ErrorType<Error> = AxiosError<Error>;
-
-const _handleRedirectByStatus = (status: number) => {
-  console.log('inside redirection handler');
-  switch (status) {
-    case 401:
-      console.log('TODO: Redirect to Login!!!');
-      break;
-    case 403:
-      console.log('TODO: handle status 403 with special header');
-      break;
-    default:
-      return;
-  }
-};
