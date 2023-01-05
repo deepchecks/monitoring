353843d9b6dd245defbe1ddece77fd94a41d27bc
Itay Gabbay <itay@deepchecks.com>
2023-01-05 17:09:39 +0200

    Integrating Sentry for frontend application errors (#1048)
    * Integrating Sentry for frontend application errors
    
    * FIX

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,11 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { hotjar } from 'react-hotjar';
 import mixpanel from 'mixpanel-browser';
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 
 import App from './App';
+
+const sentryEnv = process.env.SENTRY_ENV;
+const sentryDsn = process.env.SENTRY_DSN;
+
+Sentry.init({
+  dsn: sentryDsn,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 0.4,
+  environment: sentryEnv
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const hotjarId = process.env.REACT_APP_HJ_ID;
