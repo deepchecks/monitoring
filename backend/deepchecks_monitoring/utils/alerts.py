86632f70c9dd0d194b525729ed97528e9583ebbb
Yurii Romanyshyn <71635444+yromanyshyn@users.noreply.github.com>
2023-01-15 15:30:01 +0200

    settings refactoring (#1091)

--- backend/deepchecks_monitoring/utils/alerts.py
+++ backend/deepchecks_monitoring/utils/alerts.py
@@ -4,17 +4,16 @@ import typing as t
 from furl import furl
 
 if t.TYPE_CHECKING:
-    from deepchecks_monitoring.config import Settings  # pylint: disable=unused-import
     from deepchecks_monitoring.public_models import Alert  # pylint: disable=unused-import
 
 __all__ = ["prepare_alert_link"]
 
 
-def prepare_alert_link(alert: "Alert", settings: "Settings") -> furl:
+def prepare_alert_link(alert: "Alert", deepchecks_host: str) -> furl:
     """Return link to the given alert instance."""
     alert_rule = alert.alert_rule
     monitor = alert_rule.monitor
     check = monitor.check
     model = check.model
-    alert_link = (furl(settings.host) / "alert-rules")
+    alert_link = (furl(deepchecks_host) / "alert-rules")
     return alert_link.add({"models": model.id, "severity": alert_rule.alert_severity.value})
