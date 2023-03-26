9ec61bfaa829d1598e8d25df6a2276c722b29682
Liron Hamra <79302922+lironhamra@users.noreply.github.com>
2023-03-26 16:25:35 +0300

    MON-1823 [Block Slack OSS] (#1451)

--- frontend/src/helpers/tierControl.tsx
+++ frontend/src/helpers/tierControl.tsx
@@ -8,6 +8,17 @@ interface Dict {
   type: { [key: string]: number | boolean };
 }
 
+export const featuresList = {
+  max_models: 'max_models',
+  signup_enabled: 'signup_enabled',
+  slack_enabled: 'slack_enabled',
+  rows_per_minute: 'rows_per_minute',
+  custom_checks_enabled: 'custom_checks_enabled',
+  data_retention_months: 'data_retention_months',
+  monthly_predictions_limit: 'monthly_predictions_limit',
+  sso_enabled: 'sso_enabled'
+};
+
 const getFeaturesStatus = async ({ setFeatures }: { setFeatures: (arg: Dict['type']) => void }) => {
   try {
     const response = (await getAvailableFeaturesApiV1OrganizationAvailableFeaturesGet()) as unknown as Dict['type'];
@@ -21,18 +32,18 @@ const getFeaturesStatus = async ({ setFeatures }: { setFeatures: (arg: Dict['typ
 interface TierControlProps {
   feature: string;
   children?: ReactNode | ReactNode[];
-  showPaymentOption?: ReactNode | ReactNode[];
+  fallbackOption?: ReactNode | ReactNode[];
 }
 
-export const TierControlWrapper = ({ children, feature, showPaymentOption }: TierControlProps) => {
+export const TierControlWrapper = ({ children, feature, fallbackOption }: TierControlProps) => {
   const [features, setFeatures] = useState<Dict['type']>({});
 
   useEffect(() => void getFeaturesStatus({ setFeatures: setFeatures }), []);
 
   if (features[feature] === true) {
     return <>{children}</>;
-  } else if (showPaymentOption) {
-    return <>{showPaymentOption}</>;
+  } else if (fallbackOption) {
+    return <>{fallbackOption}</>;
   } else {
     return <></>;
   }
