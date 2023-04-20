91b3cac90cbd80f4df252e6efdbc37563e3b71fd
JKL98ISR <jonatan.lib@gmail.com>
2023-04-20 11:19:10 +0300

    version_bump

--- docs/source/user-guide/demos/plot_lending_defaults.py
+++ docs/source/user-guide/demos/plot_lending_defaults.py
@@ -320,6 +320,6 @@ model_client.log_batch_labels(sample_ids=labels_to_update, labels=[0] * len(labe
 # If you wish to delete this model from your environment, you can do that using the ``delete_model`` function.
 # CAUTION: This will delete the model, all model versions, and all associated datasets.
 
-dc_client.delete_model(model_name)
+# dc_client.delete_model(model_name)
 
 # sphinx_gallery_thumbnail_path = '_static/images/examples/lending/money.png'
