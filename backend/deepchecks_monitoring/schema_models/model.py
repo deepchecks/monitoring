4cdddf8014e0a99fc1d4f43c059eeb16b00301b1
JKL98ISR <jonatan.lib@gmail.com>
2023-04-23 14:57:42 +0300

    v1

--- backend/deepchecks_monitoring/schema_models/model.py
+++ backend/deepchecks_monitoring/schema_models/model.py
@@ -63,7 +63,6 @@ class Model(Base, MetadataMixin):
     topic_end_offset = sa.Column(sa.BigInteger, default=-1)
     timezone = sa.Column(sa.String(50), nullable=False, server_default=sa.literal("UTC"))
 
-    data_ingestion_alert_frequency = sa.Column(sa.Enum(Frequency), nullable=False, default=Frequency.DAY)
     data_ingestion_alert_label_ratio = sa.Column(sa.Float)
     data_ingestion_alert_label_count = sa.Column(sa.Integer)
     data_ingestion_alert_sample_count = sa.Column(sa.Integer)
