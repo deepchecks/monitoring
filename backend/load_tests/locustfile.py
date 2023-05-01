f234b60873350a140d314dd804db5c46115954f7
Matan Perlmutter <matan@deepchecks.com>
2023-05-01 10:46:41 +0300

    Update locust file and profiling middleware

--- backend/load_tests/locustfile.py
+++ backend/load_tests/locustfile.py
@@ -7,12 +7,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
-from locust import HttpUser, between, task
+from locust import FastHttpUser, between, task
+from gevent.pool import Group
 
 
-class User(HttpUser):
+class User(FastHttpUser):
     wait_time = between(1, 5)
+    host = 'https://app.deepchecks.com'
+    default_headers = {'Authorization': 'basic API-TOKEN'}
 
     @task
-    def run_monitor(self):
-        self.client.get("/api/v1/monitors/27/run")
+    def run_analysis(self):
+        body = {'frequency': "DAY", 'start_time': "2023-02-28T23:59:59.000Z", 'end_time': "2023-03-31T23:59:59.000Z"}
+        checks = [874, 875, 876, 877, 878, 879, 880, 881]
+        group = Group()
+        # This will spawn the number of requests needed in parallel
+        for check in checks:
+            group.spawn(lambda: self.client.post(f"/api/v1/checks/{check}/run/lookback", json=body,
+                                                 headers=self.default_headers))
+        # Once they are ready they are hit in parallel
+        group.join()
