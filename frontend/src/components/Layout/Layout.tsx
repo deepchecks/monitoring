bc5264be7cdef7f73528fabe2d76ce538b98456d
Liron Hamra <hamraliron@gmail.com>
2023-05-01 15:15:25 +0300

    mobile version layout

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Box } from '@mui/system';
+import { Menu } from '@mui/icons-material';
+import { useMediaQuery } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+
+import useUser from 'helpers/hooks/useUser';
 
 import { Sidebar } from 'components/Layout/Sidebar';
-import useUser from 'helpers/hooks/useUser';
-import { StyledContainer } from 'components/lib';
+import { StyledContainer, StyledLogo } from 'components/lib';
+import { theme } from 'components/lib/theme';
 
 const Layout = () => {
   const { isUserDetailsComplete } = useUser();
+  const [mobileBarOpen, setMobileBarOpen] = useState(false);
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   if (!isUserDetailsComplete) return null;
+
+  const handleMobileMenuClick = () => setMobileBarOpen(!mobileBarOpen);
 
   return (
     <main>
@@ -22,16 +32,38 @@ const Layout = () => {
           }
         })}
       >
-        <Box
-          sx={{
-            display: 'flex'
-          }}
-        >
-          <Sidebar />
-          <StyledContainer padding="0 35px" width="calc(100% - 237px)" type="bg" borderRadius={0}>
-            <Outlet />
-          </StyledContainer>
-        </Box>
+        {isMobile ? (
+          <Box>
+            <StyledContainer
+              type="bar"
+              height="50px"
+              width="100%"
+              borderRadius={0}
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <Menu onClick={handleMobileMenuClick} sx={{ margin: '2px' }} />
+              <StyledLogo />
+              <Drawer open={mobileBarOpen} onClose={handleMobileMenuClick} onClick={handleMobileMenuClick}>
+                <Sidebar />
+              </Drawer>
+            </StyledContainer>
+            <StyledContainer padding="0 35px" width="100%" minHeight="100vh" type="bg" borderRadius={0}>
+              <Outlet />
+            </StyledContainer>
+          </Box>
+        ) : (
+          <Box
+            sx={{
+              display: 'flex'
+            }}
+          >
+            <Sidebar />
+            <StyledContainer padding="0 35px" width="calc(100% - 237px)" type="bg" borderRadius={0}>
+              <Outlet />
+            </StyledContainer>
+          </Box>
+        )}
       </Box>
     </main>
   );
