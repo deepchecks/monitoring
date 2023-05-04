dbaaf1a8f89c85a9985678c33470d96089705541
Matan Perlmutter <matan@deepchecks.com>
2023-05-04 13:09:17 +0300

    change check of env var

--- bin/start-task-runner.sh
+++ bin/start-task-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RUN="ddtrace-run python -m deepchecks_monitoring.bgtasks.tasks_runner"
-if [[ -z "${DD_ENV}" ]]; then
+if [[ -v DD_ENV ]]; then
   RUN="ddtrace-run ${RUN}"
 fi
 eval "${RUN}"
