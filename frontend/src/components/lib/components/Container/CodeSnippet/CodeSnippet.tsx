01649a2fa76ce15fa501dfafa98716e38e3ce88a
Liron Hamra <hamraliron@gmail.com>
2023-05-09 11:26:45 +0300

    update-ds

--- frontend/src/components/lib/components/Container/CodeSnippet/CodeSnippet.tsx
+++ frontend/src/components/lib/components/Container/CodeSnippet/CodeSnippet.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { Box, BoxProps, Typography, useTheme } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 export interface CodeSnippetProps extends BoxProps {
   code: string;
+  copyBtn?: boolean;
 }
 
-export const CodeSnippet = ({ code }: CodeSnippetProps) => {
+export const CodeSnippet = ({ code, copyBtn }: CodeSnippetProps) => {
   const theme = useTheme();
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(code);
+  };
 
   return (
     <Box
@@ -21,6 +27,12 @@ export const CodeSnippet = ({ code }: CodeSnippetProps) => {
         whiteSpace: 'pre-line'
       }}
     >
+      {copyBtn && (
+        <ContentCopyIcon
+          sx={{ cursor: 'pointer', float: 'right', width: '24px', height: '24px', color: theme.palette.grey[500] }}
+          onClick={handleCopy}
+        />
+      )}
       <Typography variant="body1">{code}</Typography>
     </Box>
   );
