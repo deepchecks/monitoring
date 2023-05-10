ef2a738c1946f928a173d33ba555c9ec18eb31aa
Kirill Arkhypov <42544127+karkhypov@users.noreply.github.com>
2023-05-10 10:28:29 +0300

    Update frontend/src/components/Dashboard/MonitorList/components/DeleteMonitor/DeleteMonitor.tsx
    Co-authored-by: Liron Hamra <79302922+lironhamra@users.noreply.github.com>

--- frontend/src/components/Dashboard/MonitorList/components/DeleteMonitor/DeleteMonitor.tsx
+++ frontend/src/components/Dashboard/MonitorList/components/DeleteMonitor/DeleteMonitor.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { MonitorSchema } from 'api/generated';
 
-import { DeletionDialog as StyledDeletionDialog } from 'components/lib/components/Dialog/DeletionDialog';
+import { StyledDeletionDialog } from 'components/lib';
 
 import { constants } from '../../../dashboard.constants';
 
