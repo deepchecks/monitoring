5bd1c9b8c7c383a6d080c15b73a65465a0c1ed4a
JKL98ISR <jonatan.lib@gmail.com>
2023-05-16 11:00:53 +0300

    add roles to backend (#83)

--- backend/tests/api/test_organization_api.py
+++ backend/tests/api/test_organization_api.py
@@ -138,12 +138,15 @@ async def test_organization_members_retrieval(
 
     assert len(retrieved_members) == len(members)
 
-    for member in retrieved_members:
+    for i, member in enumerate(retrieved_members):
         assert member["id"] in members
         assert member["email"] == members[member["id"]].email
         assert member["disabled"] == members[member["id"]].disabled
         assert member["full_name"] == members[member["id"]].full_name
-        assert member["is_admin"] == members[member["id"]].is_admin
+        if i == 0:
+            assert member["roles"] == ["owner", "admin"]
+        else:
+            assert member["roles"] == []
 
 
 @pytest.mark.asyncio
@@ -239,8 +242,8 @@ def fetch_organization_members(
         assert "email" in member
         assert "full_name" in member
         assert "picture_url" in member
-        assert "is_admin" in member
         assert "last_login" in member
         assert "disabled" in member
+        assert "roles" in member
 
     return data
