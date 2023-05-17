a51934920009fa7dd6ebd01935fdcd2d9fbd92ca
JKL98ISR <jonatan.lib@gmail.com>
2023-05-17 17:00:08 +0300

    lint

--- backend/deepchecks_monitoring/ee/api/v1/__init__.py
+++ backend/deepchecks_monitoring/ee/api/v1/__init__.py
@@ -1,4 +1,4 @@
-from . import billing, slack, members
+from . import billing, members, slack
 from .routers import cloud_router, ee_router
 
 __all__ = ['cloud_router', 'ee_router', 'slack', 'billing', 'members']
