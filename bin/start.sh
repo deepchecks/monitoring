9e0e5145995b41eaedc53473fcf83760ac26a5ee
Yuriy Romanyshyn <yurii@deepchecks.com>
2023-05-26 12:17:24 +0300

    start.sh script fix

--- bin/start.sh
+++ bin/start.sh
@@ -3,8 +3,7 @@ cd /code/backend && alembic --name public upgrade head
 python /code/backend/deepchecks_monitoring/cli.py upgrade-organizations-schemas --orgid=all
 
 if [[ -v INIT_LOCAL_RAY_INSTANCE ]]; then
-  ray start --address 127.0.0.2 --port 6389 --head
-  RAY_HOST="127.0.0.2:6389"
+  ray start --port=6399 --head
 fi
 
 STARTAPP="uvicorn --factory deepchecks_monitoring.app:create_application --host 0.0.0.0 --workers 4 --log-level critical --proxy-headers --forwarded-allow-ips '*'"
