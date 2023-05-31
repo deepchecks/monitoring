df4ec66f488b59b9e8557b6af4c5981add2303a0
Liron Hamra <hamraliron@gmail.com>
2023-05-31 12:46:37 +0300

    yarn-lock

--- frontend/src/components/lib/theme/typography.ts
+++ frontend/src/components/lib/theme/typography.ts
@@ -23,43 +23,43 @@ export const typographyOptions: TypographyOptions = {
   h3: {
     fontSize: 16,
     fontWeight: 500,
-    lineHeight: '22px',
+    lineHeight: '22.4px',
     color: isDarkMode ? paletteOptions.common?.white : paletteOptions.grey?.[600]
   },
   h5: {
     fontSize: 12,
     fontWeight: 500,
-    lineHeight: '19px',
+    lineHeight: '14.1px',
     color: paletteOptions.grey?.[500]
   },
   h6: {
     fontWeight: 500,
     fontSize: '10px',
-    lineHeight: '19px'
+    lineHeight: '14px'
   },
   body1: {
     fontSize: 14,
     fontWeight: 500,
-    lineHeight: '19px',
+    lineHeight: '19.6px',
     color: isDarkMode ? paletteOptions.common?.white : paletteOptions.grey?.[600]
   },
   body2: {
     fontSize: 14,
     fontWeight: 700,
-    lineHeight: '19px',
+    lineHeight: '19.6px',
     color: paletteOptions.grey?.[500]
   },
   subtitle1: {
     fontSize: 12,
     fontWeight: 700,
-    lineHeight: '19px',
+    lineHeight: '12px',
     color: paletteOptions.grey?.[500]
   },
   subtitle2: {
     fontFamily: 'Plus Jakarta Sans, sans-serif',
     fontSize: 12,
     fontWeight: 700,
-    lineHeight: '19px',
+    lineHeight: '15.6px',
     color: paletteOptions.grey?.[400]
   },
   button: {
