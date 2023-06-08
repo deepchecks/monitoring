cd2ad0caa3137b2dbd8c07e885a64bf794ecf1b7
Liron Hamra <hamraliron@gmail.com>
2023-06-08 09:52:15 +0300

    link

--- frontend/src/components/OnBoarding/components/ColabLink.tsx
+++ frontend/src/components/OnBoarding/components/ColabLink.tsx
@@ -6,7 +6,7 @@ import logoImg from '../../../assets/onBoarding/colab.svg';
 
 const constants = {
   text: 'Open with Colab',
-  link: 'https://colab.research.google.com/drive/1M6-09zk5BI6ZrOC9Pns_yvVYRm20me5v#scrollTo=6ozMJDZcXunN'
+  link: 'https://colab.research.google.com/drive/1M6-09zk5BI6ZrOC9Pns_yvVYRm20me5v#scrollTo=3mzmr6gfYBbK'
 };
 
 const ColabLink = () => (
