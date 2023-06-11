5458868b13ca7f3e7661c6b01f5cea8a2331e095
shayts7 <shayts7@gmail.com>
2023-06-11 19:11:57 +0300

    solve linter errors (remove useless escape char)

--- frontend/src/helpers/utils/validateEmail.ts
+++ frontend/src/helpers/utils/validateEmail.ts
@@ -1,2 +1,2 @@
 // taken from: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
-export const validateEmail = (email: string) => /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+export const validateEmail = (email: string) => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
