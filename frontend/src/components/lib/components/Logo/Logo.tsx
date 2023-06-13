394c2d8ff28f7ce5bdbebd4fdd657a500b6910da
Liron Hamra <hamraliron@gmail.com>
2023-06-13 16:48:13 +0300

    styled-logo

--- frontend/src/components/lib/components/Logo/Logo.tsx
+++ frontend/src/components/lib/components/Logo/Logo.tsx
@@ -1,70 +1,24 @@
 import React from 'react';
 
-import { Box, Typography, useTheme } from '@mui/material';
-
 export interface LogoProps {
   withLabel?: boolean;
-  labelColor?: 'black' | 'white' | 'gray';
-  logoColor?: 'black' | 'white' | 'color' | 'mon';
   margin?: string;
 }
 
 export const Logo = (props: LogoProps) => {
-  const { withLabel, labelColor, logoColor, margin = '8px' } = props;
+  const { withLabel, margin = '8px' } = props;
 
-  const theme = useTheme();
-
-  const logoLabel = 'Deepchecks';
-
-  const logo = require('../../assets/logo/logo.svg').default;
-  const logoBlack = require('../../assets/logo/logo-black.svg').default;
-  const logoWhite = require('../../assets/logo/logo-white.svg').default;
-  const logoMon = require('../../assets/logo/monitoring.svg').default;
+  const logoWithoutLabel = require('../../assets/logo/logo.svg').default;
+  const logoWithLabel = require('../../assets/logo/monitoring.svg').default;
 
   const logoToUse = () => {
-    switch (logoColor) {
-      case 'color':
-        return logo;
-      case 'black':
-        return logoBlack;
-      case 'white':
-        return logoWhite;
-      case 'mon':
-        return logoMon;
+    switch (withLabel) {
+      case true:
+        return logoWithLabel;
       default:
-        return logo;
+        return logoWithoutLabel;
     }
   };
 
-  const labelColorToUse = () => {
-    switch (labelColor) {
-      case 'gray':
-        return theme.palette.grey[400];
-      case 'black':
-        return theme.palette.common.black;
-      case 'white':
-        return theme.palette.common.white;
-      default:
-        return '';
-    }
-  };
-
-  return (
-    <Box
-      sx={{
-        margin: margin,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: '16px'
-      }}
-    >
-      <img src={logoToUse()} alt="logo" height="40px" width="200px" />
-      {withLabel && (
-        <Typography fontSize={16} fontWeight={900} test-id={'logo-text'} color={labelColorToUse()}>
-          {logoLabel}
-        </Typography>
-      )}
-    </Box>
-  );
+  return <img src={logoToUse()} alt="logo" height="40px" width="200px" style={{ margin: margin }} />;
 };
