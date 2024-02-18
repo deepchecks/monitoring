0c996726c68f11161c42c935fcd7ebf3312f97c2
Liron Hamra <hamraliron@gmail.com>
2024-02-18 11:05:52 +0200

    liron/mon-2558-mon-decouple-the-date-ranges-and-the-aggregation-window-in

--- frontend/src/components/Analysis/AnalysisHeader/components/AnalysisHeaderOptions.tsx
+++ frontend/src/components/Analysis/AnalysisHeader/components/AnalysisHeaderOptions.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useContext, useEffect } from 'react';
-import dayjs from 'dayjs';
 
-import { ModelManagmentSchema, useGetModelAutoFrequencyApiV1ModelsModelIdAutoFrequencyGet } from 'api/generated';
-import { AnalysisContext, frequencyData } from 'helpers/context/AnalysisProvider';
+import dayjs from 'dayjs';
 
 import { Box, MenuItem, SelectChangeEvent } from '@mui/material';
 
 import { DateRange } from 'components/base/DateRange';
-import { CustomStyledSelect } from 'components/Select/CustomStyledSelect';
 import { SwitchButton } from 'components/base/Button/SwitchButton';
+import { CustomStyledSelect } from 'components/Select/CustomStyledSelect';
 
 import { StyledDivider } from '../AnalysisHeader.style';
+
 import { frequencyValues } from 'helpers/utils/frequency';
+import { AnalysisContext, frequencyData } from 'helpers/context/AnalysisProvider';
+import { ModelManagmentSchema, useGetModelAutoFrequencyApiV1ModelsModelIdAutoFrequencyGet } from 'api/generated';
 
 interface AnalysisHeaderOptions {
   model: ModelManagmentSchema;
@@ -21,16 +22,8 @@ const MAX_WINDOWS_COUNT = 31;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const AnalysisHeaderOptions = ({ model }: AnalysisHeaderOptions) => {
-  const {
-    compareWithPreviousPeriod,
-    setCompareWithPreviousPeriod,
-    period,
-    setPeriod,
-    frequency,
-    setFrequency
-    // compareByReference,
-    // setCompareByReference
-  } = useContext(AnalysisContext);
+  const { compareWithPreviousPeriod, setCompareWithPreviousPeriod, period, setPeriod, frequency, setFrequency } =
+    useContext(AnalysisContext);
 
   const [minDate, setMinDate] = useState<Date | null>(
     model.start_time && frequency ? dayjs.unix(model.start_time).toDate() : null
@@ -63,31 +56,9 @@ export const AnalysisHeaderOptions = ({ model }: AnalysisHeaderOptions) => {
     }
   };
 
-  const handleFrequencyChange = (event: SelectChangeEvent<unknown>) => {
-    const value = event.target.value as number;
-    setFrequency(value);
-    let windows_count = 12;
-    if (value < frequencyValues.DAY) {
-      windows_count = 24;
-    } else if (value < frequencyValues.WEEK) {
-      windows_count = 31;
-    }
-    if (period) {
-      let start_date = dayjs(period[1])
-        .subtract(value * windows_count, 'second')
-        .toDate();
-      if (model.start_time) {
-        const model_start_date = dayjs.unix(model.start_time).toDate();
-        if (model_start_date > start_date) {
-          start_date = model_start_date;
-        }
-      }
-      setPeriod([start_date, period[1]]);
-    }
-  };
+  const handleFrequencyChange = (event: SelectChangeEvent<unknown>) => setFrequency(Number(event.target.value));
 
   const handleDateChange = (startTime: Date | undefined, endTime: Date | undefined) => {
-    // limit selection to only 30 windows
     if (frequency && dayjs(startTime).isSame(dayjs(endTime))) {
       const newMin = dayjs(startTime)
         .subtract(frequency * MAX_WINDOWS_COUNT, 'second')
@@ -156,12 +127,6 @@ export const AnalysisHeaderOptions = ({ model }: AnalysisHeaderOptions) => {
           setChecked={setCompareWithPreviousPeriod}
           label="Compare with previous period"
         />
-        {/* <SwitchButton
-          checked={compareByReference}
-          setChecked={setCompareByReference}
-          label="Compare by reference"
-          sx={{ marginRight: 'auto' }}
-            /> */}
       </Box>
     </>
   );
