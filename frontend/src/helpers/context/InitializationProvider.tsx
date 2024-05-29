fec45c7bb9e3de2f333aedf02026efff69d65ae7
lironhamra <hamraliron@gmail.com>
2024-05-29 14:17:23 +0300

    liron/llm-2969-mon-fe-add-alerts-on-the-main-graph

--- frontend/src/helpers/context/InitializationProvider.tsx
+++ frontend/src/helpers/context/InitializationProvider.tsx
@@ -1,19 +1,13 @@
 import React, { ReactNode, useEffect } from 'react';
 
-import { useLocation, useNavigationType, createRoutesFromChildren, matchRoutes } from 'react-router-dom';
-
 import { hotjar } from 'react-hotjar';
 
 import mixpanel from 'mixpanel-browser';
 
-import * as Sentry from '@sentry/react';
-import { CaptureConsole } from '@sentry/integrations';
-import { BrowserTracing } from '@sentry/tracing';
-
 import useConfig from '../hooks/useConfig';
 
 const InitializationProvider = ({ children }: { children: ReactNode | ReactNode[] }) => {
-  const { hotjar_sv, hotjar_id, sentryDsn, enviroment, mixpanel_id } = useConfig() as { [key: string]: string };
+  const { hotjar_sv, hotjar_id, mixpanel_id } = useConfig() as { [key: string]: string };
 
   // HotJar
   const hotJarId = Number(hotjar_sv);
@@ -24,28 +18,6 @@ const InitializationProvider = ({ children }: { children: ReactNode | ReactNode[
       hotjar.initialize(+hotJarId, +hotJarSv);
     }
   }, [hotJarId, hotJarSv]);
-
-  // Sentry
-  Sentry.init({
-    dsn: sentryDsn,
-    integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
-          React.useEffect,
-          useLocation,
-          useNavigationType,
-          createRoutesFromChildren,
-          matchRoutes
-        )
-      }),
-      new CaptureConsole({
-        levels: ['warn', 'error', 'info']
-      })
-    ],
-    tracesSampleRate: 1,
-    environment: enviroment,
-    denyUrls: [new RegExp('https?://localhost*'), new RegExp('https?://127.0.0.1*')]
-  });
 
   // MixPanel
   const mixpanelId = mixpanel_id;
